675efcdb203743a7f22c3018de57b08aedd53ae1
berktutu <brktutu@gmail.com>
2025-12-10 20:48:31 +0300

    Class Attribute  page: added test click button xpath class and accept dialog

---
+++ tests/class_attribute.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+import { setUpUrl } from "./helpers/test-helper";
+
+test.beforeEach(async ({ page }) => {
+  await setUpUrl(page);
+  const dynamicIdPage = page.getByRole("link", { name: "Class Attribute" });
+  await dynamicIdPage.click();
+});
+
+test("Identify and click the right button", async ({ page }) => {
+  const primaryBtn = page.locator(
+    "//button[contains(concat('', normalize-space(@class), ''), 'btn-primary')]"
+  );
+
+  page.on("dialog", async (dialog) => {
+    console.log("Popup message:", dialog.message());
+    expect(dialog.message()).toBe("Primary button pressed");
+    await dialog.accept();
+  });
+
+  await primaryBtn.click();
+});
