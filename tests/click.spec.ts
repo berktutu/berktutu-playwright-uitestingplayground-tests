545db52b6751c7bcc93e05b89709ef373a89f9b4
berktutu <brktutu@gmail.com>
2025-12-12 18:30:29 +0300

    Click Page: Added test for executing the click event

---
+++ tests/click.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+import { setUpUrl } from "./helpers/test-helper";
+
+test.beforeEach(async ({ page }) => {
+  await setUpUrl(page);
+});
+
+test("Execute click event", async ({ page }) => {
+  const clickPage = page.getByRole("link", { name: "Click" });
+  await clickPage.click();
+
+  const clickEventBtn = page.getByRole("button", {
+    name: "Button That Ignores DOM Click Event",
+  });
+  await clickEventBtn.click();
+
+  // Moving the mouse away so the button is not on hover state anymore. Helps the correct color to be picked
+  await page.mouse.move(0, 0);
+
+  await expect(clickEventBtn).toHaveCSS("background-color", "rgb(40, 167, 69)");
+});
